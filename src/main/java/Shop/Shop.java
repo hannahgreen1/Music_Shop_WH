c53d656c652bd4efde261d572f6f695469b0421e
hannahgreen1 <hannahgreen94@icloud.com>
2018-05-27 22:32:53 +0100

    all money to till

--- src/main/java/Shop/Shop.java
+++ src/main/java/Shop/Shop.java
@@ -1,16 +1,30 @@
 package Shop;
 
+import Shop.instruments.Instruments;
+
+import java.util.ArrayList;
+
 public class Shop {
 
     private double till;
+    private ArrayList<Instruments> instrument;
+    Instruments instruments;
+    private double sellPrice;
 
 
-    public Shop(double till){
+    public Shop(double till, double sellPrice){
         this.till = till;
+        this.sellPrice = sellPrice;
     }
 
 
     public double getTill() {
         return this.till;
     }
+
+    public double addMoneyToTill(){
+       return this.till += sellPrice;
+    }
+
+
 }
